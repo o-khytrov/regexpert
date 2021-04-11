a6c1df7d5d62722bfec10eee4c3f95ad23aa19b7
O.Khytrov <o.khytrov@gmail.com>
2021-04-11 10:24:10 +0300

    updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import * as data from './data/tasks.json';
+import { Task } from './models/task';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'regexpert';
+  tasks: Task[] = (data as any).default;
+  currentTask: Task;
+  pattern:string;
+  str:string;
+  currentTaskIndex=0;
+  constructor()
+  {
+    console.log(data);
+    this.currentTask = this.tasks[this.currentTaskIndex];
+    
+  }
+  nextTask()
+  {
+    this.currentTaskIndex++; 
+    this.currentTask = this.tasks[this.currentTaskIndex];
+  }
 }
---
+++ src/app/models/task.ts
@@ -0,0 +1,5 @@
+export class Task
+{
+    text:string;
+    expected:string;
+}
